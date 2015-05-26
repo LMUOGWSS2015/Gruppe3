ec0aa3b55475fc7d788ca6a7fb2efcc6ee34d9e1
SaronMeb <saron@mebrahtu.net>
2015-05-26 21:06:32 +0200

    Flaslight follows mouse

--- Project_Gruppe3/Assets/FlashLight.cs
+++ Project_Gruppe3/Assets/FlashLight.cs
@@ -3,23 +3,32 @@ using System.Collections;
 
 public class FlashLight : MonoBehaviour 
 {
-	public Light light1;  
-	
+	public float distance = 10.0f;
+
 	void Start()
 	{
-		light1.enabled = false;
+
+		//light1 = GetComponentInChildren<Light>();
+		//light1.enabled = false;
 	}
-	
-	public void ToggleFlashLight () 
+/*
+	void Update()
 	{
-		
-		if (light1.enabled == true) 
-		{
-			light1.enabled = false;
-		}
-		else
-		{
+		if(Input.GetKeyDown(KeyCode.F))
+			on = !on;
+		if(on)
 			light1.enabled = true;
-		}
+		else if(!on)
+			light1.enabled = false;
 	}
+	*/
+
+
+	void Update () {
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = distance;
+		transform.position = Camera.main.ScreenToWorldPoint (mousePosition);
+	}
+
+
 }
