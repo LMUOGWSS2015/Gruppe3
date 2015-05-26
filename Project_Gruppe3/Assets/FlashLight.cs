7126ad08d38adc1da2fba3e35fc7b07a0e99bf64
SaronMeb <saron@mebrahtu.net>
2015-05-26 17:05:23 +0200

    BounsLevel Scene

---
+++ Project_Gruppe3/Assets/FlashLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour 
+{
+	public Light light1;  
+	
+	void Start()
+	{
+		light1.enabled = false;
+	}
+	
+	public void ToggleFlashLight () 
+	{
+		
+		if (light1.enabled == true) 
+		{
+			light1.enabled = false;
+		}
+		else
+		{
+			light1.enabled = true;
+		}
+	}
+}
