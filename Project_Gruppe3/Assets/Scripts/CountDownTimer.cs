116db1cc1b65e1189d8e695648fe6c74a1de53c1
bsrgltn <busragulten@hotmail.de>
2015-05-30 10:07:16 +0200

    Adding CountDownTimer

---
+++ Project_Gruppe3/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDownTimer : MonoBehaviour {
+
+	float timeRemaining = 10; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timeRemaining -= Time.deltaTime;
+	}
+
+	void OnGUI(){
+	
+		if (timeRemaining > 0) {
+			GUI.Label (new Rect (125, 25, 200, 100), "Time Remaining: " + (int)timeRemaining);
+		} else {
+			GUI.Label(new Rect(125, 25, 100, 100), "Time is up!");
+		}
+	}
+}
