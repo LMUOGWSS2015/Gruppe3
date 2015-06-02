dff38d86a1f3d43805f04b0dca64e28c45d9a113
Ina Lieder <inalieder@gmail.com>
2015-06-02 19:46:08 +0200

    enemy destroys himself on collision with waterdropbullet

---
+++ Project_Gruppe3/Assets/Scripts/enemyToKill.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyToKill : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider coll)
+	{
+		// if Enemy collides with the Bullet (waterDrop), destroy it itself
+		if (coll.gameObject.tag == "waterDrop")
+		{
+			// run destruction function
+			Destroy(this.gameObject);
+		}
+		else{
+			//do nothing
+		}
+	}
+}
