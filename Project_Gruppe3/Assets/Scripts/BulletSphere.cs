d2bef5d823b2fb4094835a848515e8303cd5226a
Ina Lieder <inalieder@gmail.com>
2015-06-02 18:42:26 +0200

    shooting function mockup added

---
+++ Project_Gruppe3/Assets/Scripts/BulletSphere.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSphere : MonoBehaviour 
+{
+	public float speedDiff;
+	public float destroySeconds;
+	
+	private Rigidbody rigid;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		// cache components
+		rigid = GetComponent<Rigidbody>();
+
+		
+		//destroy object after x seconds
+		Destroy(this.gameObject, destroySeconds);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		// add forward force to the bullet
+		rigid.velocity = transform.forward * (speedDiff);
+	}
+	
+	void OnTriggerEnter(Collider coll)
+	{
+		// if bullet collides with anything, destroy it
+		Destroy(this.gameObject);
+	}
+}
--- Project_Gruppe3/Assets/Scripts/RigPlayerMovement.cs
+++ Project_Gruppe3/Assets/Scripts/RigPlayerMovement.cs
@@ -234,19 +234,6 @@ public class RigPlayerMovement : MonoBehaviour {
 		}
 		}
 	}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
 using UnityEngine;
 using System.Collections;
 
@@ -287,25 +274,30 @@ public class RigPlayerMovement : MonoBehaviour
 	public bool canRunSidestep = true;
 	public bool canJump = true;
 	public bool canRun = true;
+
+	//Bullet-Sphere-Object
+	public GameObject bulletSphere;
+	public float bulletDistance;
 	
 	#endregion
+
 	
 	#region Unity event functions
 	
 	/// <summary>
 	/// Use for initialization
 	/// </summary>
-	void Awake()
+	void Awake ()
 	{
-		capsule = GetComponent<CapsuleCollider>();
-		GetComponent<Rigidbody>().freezeRotation = true;
-		GetComponent<Rigidbody>().useGravity = true;
+		capsule = GetComponent<CapsuleCollider> ();
+		GetComponent<Rigidbody> ().freezeRotation = true;
+		GetComponent<Rigidbody> ().useGravity = true;
 	}
 	
 	/// <summary>
 	/// Use this for initialization
 	/// </summary>
-	void Start()
+	void Start ()
 	{
 		
 	}
@@ -313,41 +305,42 @@ public class RigPlayerMovement : MonoBehaviour
 	/// <summary>
 	/// Update is called once per frame
 	/// </summary>
-	void Update()
+	void Update ()
 	{
 		// Cache the input
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown ("Jump"))
 			jumpFlag = true;
+		if (Input.GetMouseButtonDown (1)) {
+			ShootWithWater (bulletDistance);
+		}
 
 	}
 	
 	/// <summary>
 	/// Update for physics
 	/// </summary>
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
 		//Rotate field of view
 		//++++++++++++++++++++ REPLACEMENT FOR EYETRACKING ++++++++++++++++++++
 		if (Input.GetAxis ("Mouse X") < 0 || Input.GetAxis ("Mouse X") >= 0) {
 			yRot += 10 * Input.GetAxis ("Mouse X");
 		}
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, yRot, 0.0f);
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, yRot, 0.0f);
 
 		// Cache de input
-		var inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		var inputVector = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 		
 		// On the ground
-		if (grounded)
-		{
+		if (grounded) {
 			// Apply a force that attempts to reach our target velocity
-			var velocityChange = CalculateVelocityChange(inputVector);
-			GetComponent<Rigidbody>().AddForce(velocityChange, ForceMode.VelocityChange);
+			var velocityChange = CalculateVelocityChange (inputVector);
+			GetComponent<Rigidbody> ().AddForce (velocityChange, ForceMode.VelocityChange);
 			
 			// Jump
-			if (canJump && jumpFlag)
-			{
+			if (canJump && jumpFlag) {
 				jumpFlag = false;
-				GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y + CalculateJumpVerticalSpeed(), GetComponent<Rigidbody>().velocity.z);
+				GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody> ().velocity.x, GetComponent<Rigidbody> ().velocity.y + CalculateJumpVerticalSpeed (), GetComponent<Rigidbody> ().velocity.z);
 			}
 			
 			// By setting the grounded to false in every FixedUpdate we avoid
@@ -355,30 +348,48 @@ public class RigPlayerMovement : MonoBehaviour
 			grounded = false;
 		}
 		// In mid-air
-		else
-		{
+		else {
 			// Uses the input vector to affect the mid air direction
-			var velocityChange = transform.TransformDirection(inputVector) * inAirControl;
-			GetComponent<Rigidbody>().AddForce(velocityChange, ForceMode.VelocityChange);
+			var velocityChange = transform.TransformDirection (inputVector) * inAirControl;
+
+			GetComponent<Rigidbody> ().AddForce (velocityChange, ForceMode.VelocityChange);
 		}
 	}
-	
+
+	void  ShootWithWater (float sphereDistance)
+	{
+
+		Vector3 startPosition = new Vector3 (transform.position.x+0.01F,
+		                                     transform.position.y+5.2F,
+					                                   transform.position.z);
+		Vector3 velocityChangeForShot = 1.02f*(startPosition);
+
+
+		// spawn 1 kugel
+		Instantiate (bulletSphere, velocityChangeForShot, transform.rotation);
+
+
+
+
+	}
+
+
 	// Unparent if we are no longer standing on our parent
-	void OnCollisionExit(Collision collision)
+	void OnCollisionExit (Collision collision)
 	{
 		if (collision.transform == transform.parent)
 			transform.parent = null;
 	}
 	
 	// If there are collisions check if the character is grounded
-	void OnCollisionStay(Collision col)
+	void OnCollisionStay (Collision col)
 	{
-		TrackGrounded(col);
+		TrackGrounded (col);
 	}
 	
-	void OnCollisionEnter(Collision col)
+	void OnCollisionEnter (Collision col)
 	{
-		TrackGrounded(col);
+		TrackGrounded (col);
 	}
 	
 	#endregion
@@ -386,57 +397,47 @@ public class RigPlayerMovement : MonoBehaviour
 	#region Methods
 	
 	// From the user input calculate using the set up speeds the velocity change
-	private Vector3 CalculateVelocityChange(Vector3 inputVector)
+	private Vector3 CalculateVelocityChange (Vector3 inputVector)
 	{
 		// Calculate how fast we should be moving
-		var relativeVelocity = transform.TransformDirection(inputVector);
-		if (inputVector.z > 0)
-		{
-			relativeVelocity.z *= (canRun && Input.GetButton("Jump")) ? runSpeed : walkSpeed;
+		var relativeVelocity = transform.TransformDirection (inputVector);
+		if (inputVector.z > 0) {
+			relativeVelocity.z *= (canRun && Input.GetButton ("Jump")) ? runSpeed : walkSpeed;
+		} else {
+			relativeVelocity.z *= (canRun && Input.GetButton ("Jump")) ? runBackwardSpeed : walkBackwardSpeed;
 		}
-		else
-		{
-			relativeVelocity.z *= (canRun && Input.GetButton("Jump")) ? runBackwardSpeed : walkBackwardSpeed;
-		}
-		relativeVelocity.x *= (canRunSidestep && Input.GetButton("Jump")) ? runSidestepSpeed : sidestepSpeed;
+		relativeVelocity.x *= (canRunSidestep && Input.GetButton ("Jump")) ? runSidestepSpeed : sidestepSpeed;
 		
 		// Calcualte the delta velocity
-		var currRelativeVelocity = GetComponent<Rigidbody>().velocity - groundVelocity;
+		var currRelativeVelocity = GetComponent<Rigidbody> ().velocity - groundVelocity;
 		var velocityChange = relativeVelocity - currRelativeVelocity;
-		velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-		velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+		velocityChange.x = Mathf.Clamp (velocityChange.x, -maxVelocityChange, maxVelocityChange);
+		velocityChange.z = Mathf.Clamp (velocityChange.z, -maxVelocityChange, maxVelocityChange);
 		velocityChange.y = 0;
 		
 		return velocityChange;
 	}
 	
 	// From the jump height and gravity we deduce the upwards speed for the character to reach at the apex.
-	private float CalculateJumpVerticalSpeed()
+	private float CalculateJumpVerticalSpeed ()
 	{
-		return Mathf.Sqrt(2f * jumpHeight * Mathf.Abs(Physics.gravity.y));
+		return Mathf.Sqrt (2f * jumpHeight * Mathf.Abs (Physics.gravity.y));
 	}
 	
 	// Check if the base of the capsule is colliding to track if it's grounded
-	private void TrackGrounded(Collision collision)
+	private void TrackGrounded (Collision collision)
 	{
 		var maxHeight = capsule.bounds.min.y + capsule.radius * .9f;
-		foreach (var contact in collision.contacts)
-		{
-			if (contact.point.y < maxHeight)
-			{
-				if (isKinematic(collision))
-				{
+		foreach (var contact in collision.contacts) {
+			if (contact.point.y < maxHeight) {
+				if (isKinematic (collision)) {
 					// Get the ground velocity and we parent to it
 					groundVelocity = collision.rigidbody.velocity;
 					transform.parent = collision.transform;
-				}
-				else if (isStatic(collision))
-				{
+				} else if (isStatic (collision)) {
 					// Just parent to it since it's static
 					transform.parent = collision.transform;
-				}
-				else
-				{
+				} else {
 					// We are standing over a dinamic object,
 					// set the groundVelocity to Zero to avoid jiggers and extreme accelerations
 					groundVelocity = Vector3.zero;
@@ -447,22 +448,22 @@ public class RigPlayerMovement : MonoBehaviour
 		}
 	}
 	
-	private bool isKinematic(Collision collision)
+	private bool isKinematic (Collision collision)
 	{
-		return isKinematic(GetComponent<Collider>().transform);
+		return isKinematic (GetComponent<Collider> ().transform);
 	}
 	
-	private bool isKinematic(Transform transform)
+	private bool isKinematic (Transform transform)
 	{
-		return transform.GetComponent<Rigidbody>() && transform.GetComponent<Rigidbody>().isKinematic;
+		return transform.GetComponent<Rigidbody> () && transform.GetComponent<Rigidbody> ().isKinematic;
 	}
 	
-	private bool isStatic(Collision collision)
+	private bool isStatic (Collision collision)
 	{
-		return isStatic(collision.transform);
+		return isStatic (collision.transform);
 	}
 	
-	private bool isStatic(Transform transform)
+	private bool isStatic (Transform transform)
 	{
 		return transform.gameObject.isStatic;
 	}
