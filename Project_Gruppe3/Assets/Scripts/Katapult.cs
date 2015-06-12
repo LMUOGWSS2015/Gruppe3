c2bcaf3954d935e0aa665716d023c8c08f9cb038
SaronMeb <saron@mebrahtu.net>
2015-06-12 09:06:24 +0200

    Catapult Function
    Vorerst nur ganz einfach nach vorne bewegen des Hundes Ausgelöst durch
    Space Taste. Sobald entschieden wie Katapult aussieht -> Animation.
    Auslösen des Fluges durch Eyetracking fehlt noch

---
+++ Project_Gruppe3/Assets/Scripts/Katapult.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using iView;
+
+public class Katapult : GazeMonobehaviour {
+	Rigidbody dogRB;
+	
+	// Use this for initialization
+	void Start () {
+		dogRB = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			dogRB.AddForce (Vector3.forward * 300);
+			dogRB.useGravity = false;
+		}
+
+
+
+	}
+	//Eyetracking
+
+}
