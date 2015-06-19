f2da6cf3cdd9f36909e286d030918e045c652549
ZenibAwan <Zenib@dhcp-10-176-12-81.dynamic.eduroam.mwn.de>
2015-06-19 16:25:25 +0200

    Cube to get to the next level added

---
+++ Project_Gruppe3/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col){
+		
+		// Enemy hits Player --> Player gettin hurt
+		if (col.gameObject.name == "Player") {
+			Debug.Log("NextLevel_Collision");
+			Destroy(gameObject);
+
+			Application.LoadLevel("Menu");
+		
+		}
+	}
+}
