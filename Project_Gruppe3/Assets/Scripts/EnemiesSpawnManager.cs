68c68589bcf880869883d45b1220885e3e267814
Ina Lieder <inalieder@gmail.com>
2015-06-24 10:54:00 +0200

    LightMap settings changed(no more automatically build with weird effects ( hopefully ) ) HelmetSpawner Script and object added Correction of EnemiesSpawner Manager(Destroy of EnemySpawnPoint instance will be self-destroyed now)

--- Project_Gruppe3/Assets/Scripts/EnemiesSpawnManager.cs
+++ Project_Gruppe3/Assets/Scripts/EnemiesSpawnManager.cs
@@ -14,6 +14,10 @@ public class EnemiesSpawnManager : MonoBehaviour {
 	#region Variables (private)
 
 	private GameObject[] enemyPoints;
+	private GameObject pug;
+
+
+	
 
 	#endregion
 	
@@ -22,23 +26,42 @@ public class EnemiesSpawnManager : MonoBehaviour {
 
 	void Update(){
 		// Get all Spawning Points
+		pug=GameObject.FindGameObjectWithTag("pug");
+
 		if(enemyPoints == null)
 			enemyPoints = GameObject.FindGameObjectsWithTag ("EnemySpawnPoint");
 
+			
+
+
+
 		// Create enemy for each point an enemy
-		foreach (GameObject enemyPoint in enemyPoints) {
-			float distance = Vector3.Distance (enemyPoint.transform.position, player.transform.position);
+		//foreach (GameObject enemyPoint in enemyPoints) {
+		//	float distance = Vector3.Distance (enemyPoint.transform.position, player.transform.position);
 			//Debug.Log (distance);
 
-			if (distance < 10f){
-			
-				CreateEnemy ();
+		//	if ((distance <10f)&&(distance>9)){
+				//enemyPoint.SetActive(false);
+		//		CreateEnemy ();
+				//Destroy(enemyPoint);
 
-				// Disablin the spawning point ('Destroy' didnt work)
-				enemyPoint.transform.position = new Vector3 (enemyPoint.transform.position.x,
-				                                             enemyPoint.transform.position.y - 100f,
-				                                             enemyPoint.transform.position.z);
-			} 
+
+				//Disablin the spawning point ('Destroy' didnt work)
+				//enemyPoint.transform.position = new Vector3 (enemyPoint.transform.position.x,
+				  //                                          enemyPoint.transform.position.y - 100f,
+				    //                                         enemyPoint.transform.position.z);
+			//}
+
+
+
+
+		//}
+		//Destroy is now Working 
+		float distance = Vector3.Distance (this.gameObject.transform.position, pug.transform.position);
+		Debug.Log ( distance +  this.gameObject.ToString() + "blla" + this.gameObject.transform.localPosition.ToString()+"bla"+pug.transform.position);
+		if (distance < 10f) {
+			CreateEnemy();
+			Destroy(this.gameObject);
 		}
 	}
 
@@ -51,7 +74,7 @@ public class EnemiesSpawnManager : MonoBehaviour {
 
 		// Creatin enemy infront of the player's view
 		Instantiate(newEnemy, 
-		            player.transform.position + player.transform.forward * 40f,
+		            pug.transform.position + pug.transform.forward * 30f,
 		            new Quaternion( 0.0f, player.transform.rotation.y, 0.0f, player.transform.rotation.w));
 	}
 
---
+++ Project_Gruppe3/Assets/Scripts/HelmetSpawnManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmetSpawnManager : MonoBehaviour
+{
+
+#region Variables (public)
+		
+public GameObject Helmet;
+
+		
+#endregion
+		
+		
+#region Variables (private)
+		
+private GameObject pug;
+		
+#endregion
+		
+		
+#region Unity event functions
+		
+	void Update ()
+	{
+		// Get all Spawning Points
+		pug = GameObject.FindGameObjectWithTag ("pug");
+
+		float distance = Vector3.Distance (this.gameObject.transform.position, pug.transform.position);
+		if (distance < 20f) {
+			CreateHelmet ();
+			Destroy (this.gameObject);
+		}
+	}
+		
+		#endregion
+		
+		
+		#region Methods
+		
+	void CreateHelmet ()
+	{
+			
+		// Creatin enemy infront of the player's view
+		Instantiate (Helmet, 
+			            pug.transform.position + pug.transform.forward * 30f,
+			            new Quaternion (0.0f, pug.transform.rotation.y, 0.0f, pug.transform.rotation.w));
+	}
+		
+		#endregion
+}
+	
+	
+	
+	
