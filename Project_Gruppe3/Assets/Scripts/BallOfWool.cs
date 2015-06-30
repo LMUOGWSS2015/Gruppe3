c4c59f2ce4c0a5cb5f543786ac94174ecab0ef64
ZenibAwan <Zenib@Zenibs-MacBook-Pro.local>
2015-06-30 16:58:45 +0200

    Creatin BallOfWoolSpawnPoint, etc.

---
+++ Project_Gruppe3/Assets/Scripts/BallOfWool.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallOfWool : MonoBehaviour {
+	
+	#region Variables (public)
+	
+	//public static bool isHurt = false;
+	//public PugLife pugLife;
+	public GameObject dogsBone;
+	//	public int numberOfObjects;
+	public int numbOfObj;
+	public float radius = 1f;
+	//public RaycastHit hit;
+	Vector3 startPosition;
+	
+	#endregion
+	
+	
+	#region Methods
+	
+	void OnCollisionEnter (Collision col){
+		
+		// Enemy hits Player --> Player gettin hurt
+		if (col.gameObject.name == "Player"){
+			Debug.Log("-1 life");
+			//isHurt = true;
+			
+			//pugLife.DecreaseLife();
+			GameObject.FindGameObjectWithTag("pug").GetComponent<PugLife>().DecreaseLife();
+		}
+	}
+	
+	void OnTriggerEnter (Collider coll){
+		
+		// if Enemy collides with the Bullet (waterDrop), destroy it itself
+		if (coll.gameObject.tag == "waterDrop") {
+			
+			startPosition = coll.transform.position;
+			// run destruction function
+			Destroy (this.gameObject);
+			
+			// Adding score points for killing an enemies
+			GameObject.FindGameObjectWithTag ("pug").GetComponent<Score> ().score += 100;
+			HoldInformations.SetScore(GameObject.FindGameObjectWithTag ("pug").GetComponent<Score> ().score);
+			
+		} 
+	}
+
+	#endregion
+}
---
+++ Project_Gruppe3/Assets/Scripts/BallOfWoolSpawnManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallOfWoolSpawnManager : MonoBehaviour {
+	
+	#region Variables (public)
+	
+	public GameObject newBallOfWool;
+	public GameObject player;
+	
+	#endregion
+	
+	
+	#region Variables (private)
+	
+	private GameObject[] ballOfWoolPoints;
+	private GameObject pug;
+	
+	#endregion
+	
+	
+	#region Unity event functions
+	
+	void Update(){
+		// Get all Spawning Points
+		pug=GameObject.FindGameObjectWithTag("pug");
+		
+		if(ballOfWoolPoints == null)
+			ballOfWoolPoints = GameObject.FindGameObjectsWithTag ("BallOfWoolSpawnPoint");
+
+		//Destroy is now Working 
+		float distance = Vector3.Distance (this.gameObject.transform.position, pug.transform.position);
+		if (distance < 10f) {
+			CreateEnemy();
+			Destroy(this.gameObject);
+		}
+	}
+	
+	#endregion
+	
+	
+	#region Methods
+	
+	void CreateEnemy(){
+		
+		// Creatin enemy infront of the player's view
+		Instantiate(newBallOfWool, 
+		            pug.transform.position + pug.transform.forward * 30f,
+		            new Quaternion( 0.0f, player.transform.rotation.y, 0.0f, player.transform.rotation.w));
+	}
+	
+	#endregion
+}
+
+
+
+
