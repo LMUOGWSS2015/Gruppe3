408e5ae406f16a9d4942d598c478dbd43b208feb
bsrgltn <busragulten@hotmail.de>
2015-07-08 19:45:35 +0200

    GazeEnter on BonusDoor

--- Project_Gruppe3/Assets/Scripts/BonusDoor.cs
+++ Project_Gruppe3/Assets/Scripts/BonusDoor.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using iView
 
-public class BonusDoor : MonoBehaviour {
+public class BonusDoor : GazeMonobehaviour {
 
 	#region Variables (private)
-	
+
+	Ray rayGaze;
+	RaycastHit hit;
 	private GameObject[] doorPoint;
 	private int length;
 	private int index;
+	public Vector3 gazePosition;
+
 
 	#endregion
 
@@ -20,5 +25,27 @@ public class BonusDoor : MonoBehaviour {
 		index = Random.Range (0, doorPoint.Length);
 		Debug.Log ("doornumber " + index);
 		doorPoint [index].GetComponent<MeshRenderer>().enabled = true;
+	}
+
+	void Update(){
+		gazePosition = SMIGazeController.Instance.GetSample ().averagedEye.gazePosInUnityScreenCoords ();
+		rayGaze = Camera.main.ScreenPointToRay (gazePosition);
+
+		if (Physics.Raycast (rayGaze, out hit)) {
+
+			base.OnGazeEnter(hit);
+		}
+	}
+
+	public override void OnGazeEnter(RaycastHit hitInformation){
+	
+		foreach(GameObject obj in doorPoint){
+			
+			if(hit.collider.gameObject.name == obj.transform.name){
+				Debug.Log ("The doornumber is: " + hit.collider.gameObject.name);
+				Application.LoadLevel("BonusLevelNew");
+
+			}
+		}
 	}
 }
