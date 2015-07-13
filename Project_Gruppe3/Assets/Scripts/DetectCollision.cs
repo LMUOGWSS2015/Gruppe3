c8e57bc876068f113a9f08ebf2b913019ba17293
SaronMeb <saron@mebrahtu.net>
2015-07-13 14:25:40 +0200

    CatapultCatAnim added

--- Project_Gruppe3/Assets/Scripts/DetectCollision.cs
+++ Project_Gruppe3/Assets/Scripts/DetectCollision.cs
@@ -2,72 +2,52 @@
 using System.Collections;
 
 public class DetectCollision : MonoBehaviour {
-	public float timeLimit = 10.0f;
 	public int lives;
 	public float restartDelay = 3f;
 	public float restartTimer;
-
-
+	
+	
 	private Animator anim;
-
+	
 	void Awake()
 	{
 		anim = GameObject.Find ("HUDCanvas").GetComponent<Animator>();
 		
 	}
 	public float timelimitLevel = 20.0f;
-
+	
 	// Update is called once per frame
 	void Update () {
-
-		timeLimit -= Time.deltaTime;
-	
-		this.timelimitLevel -= Time.deltaTime;
+		
 	}
-
-	void OnCollisionEnter(Collision col){
 	
-		//detect "collision" with target
-		if (col.gameObject.name == "Target" && (timeLimit > 0)) {
-			//detect collision with target
-			if (col.gameObject.name == "Target" && (timelimitLevel > 0)) {
-				//destroy target-wall
-				Destroy (col.gameObject); 
-
-				//back to main game
-				Application.LoadLevel ("LastScene");
-
-			}
-		//TODO
+	void OnCollisionEnter(Collision col){
+		//detect collision with target
+		if (col.gameObject.name == "HUDCanvas/Target") {
+			//destroy target-wall
+			Destroy (col.gameObject); 
+			
+			//back to main game
+			Application.LoadLevel ("LastScene");
+			
+		}
 		//detect collision with cats
 		else if (col.gameObject.name == "Cats") {
-				Debug.Log ("-1 Live");
-				decreaseLive ();
-				//reset scene#
-				ResetScene (); 
-				//TODO
-				//life -1
-			}
+			Debug.Log ("-1 Live");
+			decreaseLive ();
+			//reset scene#
+			ResetScene (); 
+			//TODO
+			//life -1
 		}
+		
 	}
-
-	//Zeitdisplay in der GUI :) 
-	void OnGUI(){
-		if (timelimitLevel  > 0) {
-			GUI.Label (new Rect (125, 25, 200, 100), "Time Remaining: " + (int)timelimitLevel );
-		} else {
-			GUI.Label(new Rect(125, 25, 100, 100), "Time is up!");
-			decreaseLive();
-		//	ResetScene(); 
-		}
-	}
-
+	
 	//reset scene
 	void ResetScene(){
-	
 		Application.LoadLevel(Application.loadedLevel);
 	}
-
+	
 	// one live less
 	public void decreaseLive (){
 		lives = HoldInformations.GetLife()- 1; 
@@ -79,17 +59,12 @@ public class DetectCollision : MonoBehaviour {
 			Application.LoadLevel("NovaMenu");
 			
 			HoldInformations.SetLife (1);
-			/*
-  				restartTimer+= Time.deltaTime;
-				if(restartTimer >= restartDelay){
-					Application.LoadLevel("NovaMenu");
-				}
-*/
+			
 		} else {
 			//reset scene
 			ResetScene (); 
 		}
 	}
-
-
+	
+	
 }
--- Project_Gruppe3/Assets/Scripts/Katapult.cs
+++ Project_Gruppe3/Assets/Scripts/Katapult.cs
@@ -12,41 +12,53 @@ public class Katapult : GazeMonobehaviour {
 	GameObject catapult;
 	Animator startCatapult;
 	Animator startDogFly;
-
-
+	Animation anim;
+	bool testBool= false;
+	
 	// Use this for initialization
 	void Start () {
 		dog = GameObject.Find ("NovaPugCatapultScene"); 
 		dogRB = dog.GetComponent<Rigidbody>(); 
 		startDogFly = dog.GetComponent<Animator>();
-
+		
 		catapultArm = GameObject.Find ("Catapuly/CatapultHeadHolder"); 
 		catapultArmRB = catapultArm.GetComponent<Rigidbody>(); 
-
+		
 		catapult = GameObject.Find ("Catapuly"); 
 		startCatapult = catapult.GetComponent<Animator>();
+		anim = dog.GetComponent<Animation> ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		rotateMouse ();
 		jump ();
-
+		if (testBool) {
+			if (!anim.isPlaying) {
+				Debug.Log (anim.isPlaying);
+				dogRB.AddForce (transform.forward * 400);
+				dogRB.useGravity = false;
+			}
+		}
+		
 	}
 	//Eyetracking
 	public void jump (){
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			startCatapult.SetTrigger("JumpTrigger");
 			startDogFly.SetTrigger("FlyTrigger");
-			//dogRB.AddForce (transform.forward * 400);
 			Debug.Log("JUMP");
-			dogRB.useGravity = false;
+			testBool= true;
+			
 		}
 	}
-
+	
+	
+	
 	public void rotateMouse (){
 		dogRB.rotation = Quaternion.Euler(dogRB.rotation.eulerAngles + new Vector3(0f, rotationSpeedMouse* Input.GetAxis ("Mouse X"), 0f));
-	//	catapultArm.transform.Rotate= Quaternion.Euler(catapultArmRB.rotation.eulerAngles + new Vector3(0f, rotationSpeedMouse* Input.GetAxis ("Mouse X"), 0f));
+		//	catapultArm.transform.Rotate= Quaternion.Euler(catapultArmRB.rotation.eulerAngles + new Vector3(0f, rotationSpeedMouse* Input.GetAxis ("Mouse X"), 0f));
 		//catapultArmRB.rotation = Quaternion.Euler(catapultArmRB.rotation.eulerAngles + new Vector3(0f, rotationSpeedMouse* Input.GetAxis ("Mouse X"), 0f));
 	}
 	//Eyetracking
@@ -54,5 +66,5 @@ public class Katapult : GazeMonobehaviour {
 		Vector3 gazePosition = SMIGazeController.Instance.GetSample ().averagedEye.gazePosInUnityScreenCoords ();
 		dogRB.rotation = Quaternion.Euler(dogRB.rotation.eulerAngles + new Vector3(0f, rotationSpeedEyes* gazePosition.x, 0f));
 	}
-
+	
 }
