c5c84b8df38d27c64095ebeac51eea6b360c6e50
blackMayo <ioanna.vletsou@mail.com>
2015-07-13 13:12:51 +0200

    Revert "DogAnimation"
    This reverts commit 8bb7ca4c9e09685036f32997ec14aa1b594f4f2d.

--- Project_Gruppe3/Assets/Scripts/DetectCollision.cs
+++ Project_Gruppe3/Assets/Scripts/DetectCollision.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class DetectCollision : MonoBehaviour {
+	public float timeLimit = 10.0f;
 	public int lives;
 	public float restartDelay = 3f;
 	public float restartTimer;
@@ -19,11 +20,17 @@ public class DetectCollision : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		timeLimit -= Time.deltaTime;
+	
+		this.timelimitLevel -= Time.deltaTime;
 	}
 
 	void OnCollisionEnter(Collision col){
+	
+		//detect "collision" with target
+		if (col.gameObject.name == "Target" && (timeLimit > 0)) {
 			//detect collision with target
-			if (col.gameObject.name == "HUDCanvas/Target") {
+			if (col.gameObject.name == "Target" && (timelimitLevel > 0)) {
 				//destroy target-wall
 				Destroy (col.gameObject); 
 
@@ -31,20 +38,33 @@ public class DetectCollision : MonoBehaviour {
 				Application.LoadLevel ("LastScene");
 
 			}
-			//detect collision with cats
-			else if (col.gameObject.name == "Cats") {
-					Debug.Log ("-1 Live");
-					decreaseLive ();
-					//reset scene#
-					ResetScene (); 
-					//TODO
-					//life -1
-				}
+		//TODO
+		//detect collision with cats
+		else if (col.gameObject.name == "Cats") {
+				Debug.Log ("-1 Live");
+				decreaseLive ();
+				//reset scene#
+				ResetScene (); 
+				//TODO
+				//life -1
+			}
+		}
+	}
 
+	//Zeitdisplay in der GUI :) 
+	void OnGUI(){
+		if (timelimitLevel  > 0) {
+			GUI.Label (new Rect (125, 25, 200, 100), "Time Remaining: " + (int)timelimitLevel );
+		} else {
+			GUI.Label(new Rect(125, 25, 100, 100), "Time is up!");
+			decreaseLive();
+		//	ResetScene(); 
+		}
 	}
 
 	//reset scene
 	void ResetScene(){
+	
 		Application.LoadLevel(Application.loadedLevel);
 	}
 
@@ -59,7 +79,12 @@ public class DetectCollision : MonoBehaviour {
 			Application.LoadLevel("NovaMenu");
 			
 			HoldInformations.SetLife (1);
-
+			/*
+  				restartTimer+= Time.deltaTime;
+				if(restartTimer >= restartDelay){
+					Application.LoadLevel("NovaMenu");
+				}
+*/
 		} else {
 			//reset scene
 			ResetScene (); 
